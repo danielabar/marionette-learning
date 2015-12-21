92f8b7693203051faaa5ce2d51cbd7f183f5b8a5
Daniela Baron <dbaron@uncharted.software>
2015-12-21 10:26:40 -0500

    refactor dupe code in user router for fetching collection and user

--- marionette-intro/app/scripts/apps/user-admin/user/router.js
+++ marionette-intro/app/scripts/apps/user-admin/user/router.js
@@ -14,25 +14,25 @@ var UserRouter = Backbone.Router.extend({
     this.module.app.trigger('user:listing:requested');
   },
 
-  // this is only called when user details view is refreshed in browser
+  // this is only called when user editing view is refreshed in browser
   showUserDetail: function(id) {
-    var self = this;
-
-    // Need to fetch entire user collection because this app depends on user events bubbling up to collection for handling
-    this.module.collection.fetch().then(function() {
-      var user = self.module.collection.get(id);
-      self.module.app.trigger('user:selected', user);
+    this.fetchAndThen(id, function(user) {
+      user.select();
     });
   },
 
   // this is only called when user editing view is refreshed in browser
   showUserEditor: function(id) {
-    // Note some code duplication with showUserDetail
-    var self = this;
+    this.fetchAndThen(id, function(user) {
+      user.edit();
+    });
+  },
 
+  fetchAndThen: function(id, next) {
+    var self = this;
     this.module.collection.fetch().then(function() {
       var user = self.module.collection.get(id);
-      self.module.app.trigger('user:editing', user);
+      next(user);
     });
   }
 
